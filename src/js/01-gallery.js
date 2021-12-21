3c4f2d563662b6dd9e9b19e87e3431688f115bc6
VladyslavGolovko <86726358+VladyslavGolovko@users.noreply.github.com>
2021-12-21 11:58:31 +0200

    made some changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryCardMarkup = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src='${item.preview}'
+      data-source='${item.original}'
+      alt='${item.description}'
+    />
+  </a>
+</div>`,
+  )
+  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardMarkup);
+
+let image = '';
+
+function createImgInModal(event) {
+  event.preventDefault();
+  image = basicLightbox.create(
+    `<div class="modal"><img src="${event.target.dataset.source}"></div>`,
+  );
+  image.show();
+}
+
+galleryContainer.addEventListener('click', createImgInModal);
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    image.close();
+  }
+}
+galleryContainer.addEventListener('keydown', onEscKeyPress);
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(sec => {
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
+    });
+  }, 1000),
+);
+
+const timeToStart = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+player.setCurrentTime(timeToStart);
